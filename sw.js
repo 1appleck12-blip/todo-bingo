55f1ad00b7ebeac65b7c5f0561baf04d70b4c70c
1appleck12-blip <1apple.ck12@gmail.com>
2026-09-23 08:26:52 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,32 +1,15 @@
-const CACHE_NAME = 'todo-bingo-v3';
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.add('./'))
-  );
-  self.skipWaiting();
+const CACHE_NAME = 'todo-bingo-v4';
+const URLS = ['./', './index.html'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(URLS)).then(() => self.skipWaiting()));
 });
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cached) => {
-      if (cached) return cached;
-      return fetch(event.request)
-        .then((response) => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
-          return response;
-        })
-        .catch(() => cached);
-    })
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).then(r => {
+    const clone = r.clone();
+    caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+    return r;
+  }).catch(() => caches.match(e.request)));
 });
